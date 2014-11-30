725d9ffcbba2a71331845f31cdbfc6b022a48247
Ganesh Satpute <ghsatpute@gmail.com>
2014-11-30 22:16:00 +0530

    Quick Find Eager Approach implemented

---
+++ src/MainClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ganesh Satpute
+ *
+ */
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
